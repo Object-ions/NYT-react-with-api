be1a0d6519e76bc84b4f63772df4673cc4c4a5db
object-ions <moshikoatia@gmail.com>
2023-12-11 14:30:17 -0800

    Update components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TopStories from "./TopStories";
+
+function App() {
+  return (
+    <>
+      <TopStories>TopStories</TopStories>
+    </>
+  );
+}
+
+export default App;
