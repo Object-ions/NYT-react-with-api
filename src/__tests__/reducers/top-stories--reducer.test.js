37cd3f36075ffc29750c230761291bb8fb84e3f5
object-ions <moshikoatia@gmail.com>
2023-12-12 18:21:09 -0800

    Run a test

--- src/__tests__/reducers/top-stories--reducer.test.js
+++ src/__tests__/reducers/top-stories--reducer.test.js
@@ -1,6 +1,9 @@
 import topStoriesReducer from "../../reducers/top-stories-reducer";
+import * as c from "./../actions/ActionTypes";
 
 describe("topStoriesReducer", () => {
+  let action;
+
   const initialState = {
     isLoaded: false,
     topStories: [],
@@ -11,5 +14,19 @@ describe("topStoriesReducer", () => {
     expect(() => {
       topStoriesReducer(initialState, { type: null });
     }).toThrowError("There is no action matching null");
+  });
+
+  test("should get top stories and change isLoaded to true and update topStories", () => {
+    const topStories = "An article";
+    action = {
+      type: c.GET_TOP_STORIES_SUCCESS,
+      topStories,
+    };
+
+    expect(topStoriesReducer(initialState, action)).toEqual({
+      isLoaded: true,
+      topStories: "An article",
+      error: null,
+    });
   });
 });
